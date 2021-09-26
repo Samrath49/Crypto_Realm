710566b8d3b7c94c47805d6dc2020c54ee33a6a2
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-09-26 12:12:35 +0530

    Resolved little typo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
-import { Navbar } from './components';
+import { Navbar, Exchanges, Homepage, CryptoDetails, Cryptocurrencies, News } from './components';
 import './App.css';
 
 const App = () => {
