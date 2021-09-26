7c138303831afa8da9836077d6d7d8103a9d17f7
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-09-26 16:37:09 +0530

    Redux Toolkit Implementation

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react';
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import { Card, Row, Col, Input } from 'antd';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const Cryptocurrencies = () => {
     return (
