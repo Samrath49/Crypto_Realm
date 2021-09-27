205de0a2b373e4ad76f65c6d1596c7dc6c7925ff
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-09-27 16:24:24 +0530

    Loader in News Component

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -15,7 +15,7 @@ const News = ({ simplified }) => {
     const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 })
     const { data } = useGetCryptosQuery(100);
 
-    if (!cryptoNews?.value) return "Loading...";
+    if (!cryptoNews?.value) return <Loader />;
     return (
         <Row gutter={[24, 24]}>
             {!simplified && (
