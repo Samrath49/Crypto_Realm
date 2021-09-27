f5547ebb33037cd176a56bf7f5d33e71c5e4461d
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-09-27 13:23:57 +0530

    Setting api in exchanges

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React from 'react';
+import millify from 'millify';
+import { Collapse, Row, Col, Typography, Avatar } from 'antd';
+import HTMLReactParser from 'html-react-parser';
 
+import { useGetExchangesQuery } from '../services/cryptoApi';
 const Exchanges = () => {
     return (
         <div>
