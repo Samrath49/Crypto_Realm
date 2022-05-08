14a3983d72eb3a476cb8f13097ba630ff54f6ff5
Samrath Chauhan <55148801+Samrath49@users.noreply.github.com>
2022-05-08 17:01:00 +0530

    Multiple changes

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,58 +1,90 @@
-import React from 'react';
-import { Line } from 'react-chartjs-2';
-import { Col, Row, Typography } from 'antd';
+import React from "react";
+import { Line } from "react-chartjs-2";
+import { Col, Row, Typography } from "antd";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-const { Title } = Typography;
+const { Title: AntTitle } = Typography;
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
-    const coinPrice = [];
-    const coinTimestamp = [];
+  const coinPrice = [];
+  const coinTimestamp = [];
 
-    for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-        coinPrice.push(coinHistory?.data?.history[i].price);
-    }
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinPrice.unshift(coinHistory?.data?.history[i].price);
+  }
 
-    for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-        coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
-    }
-
-    const data = {
-        labels: coinTimestamp,
-        datasets: [
-            {
-                label: 'Price In USD',
-                data: coinPrice,
-                fill: false,
-                backgroundColor: '#0071bd',
-                borderColor: '#0071bd',
-            },
-        ],
-    };
-
-    const options = {
-        scales: {
-            yAxes: [
-                {
-                    ticks: {
-                        beginAtZero: true,
-                    },
-                },
-            ],
-        },
-    };
-
-    return (
-        <>
-            <Row className="chart-header">
-                <Title level={2} className="chart-title">{coinName} Price Chart </Title>
-                <Col className="price-container">
-                    <Title level={5} className="price-change">Change: {coinHistory?.data?.change}%</Title>
-                    <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
-                </Col>
-            </Row>
-            <Line data={data} options={options} />
-        </>
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinTimestamp.push(
+      new Date(
+        coinHistory?.data?.history[i].timestamp * 1000
+      ).toLocaleDateString()
     );
+  }
+
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: "Price In USD",
+        data: coinPrice,
+        fill: false,
+        backgroundColor: "#0071bd",
+        borderColor: "#0071bd",
+      },
+    ],
+  };
+
+  const options = {
+    type: "line",
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  };
+
+  return (
+    <>
+      <Row className="chart-header">
+        <AntTitle level={2} className="chart-title">
+          {coinName} Price Chart{" "}
+        </AntTitle>
+        <Col className="price-container">
+          <AntTitle level={5} className="price-change">
+            Change: {coinHistory?.data?.change}%
+          </AntTitle>
+          <AntTitle level={5} className="current-price">
+            Current {coinName} Price: $ {currentPrice}
+          </AntTitle>
+        </Col>
+      </Row>
+      <Line data={data} options={options} />
+    </>
+  );
 };
 
 export default LineChart;
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -15,14 +15,14 @@ export const cryptoApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: (count) => createRequest(`/coins`),
+      query: (count) => createRequest(`/coins?limit=${count}`),
     }),
     getCryptoDetails: builder.query({
       query: (coinId) => createRequest(`/coin/${coinId}`),
     }),
     getCryptoHistory: builder.query({
       query: ({ coinId, timePeriod }) =>
-        createRequest(`/coin/${coinId}/history/${timePeriod}`),
+        createRequest(`coin/${coinId}/history?timeperiod=${timePeriod}`),
     }),
     getExchanges: builder.query({
       query: () => createRequest(`/exchanges`),
